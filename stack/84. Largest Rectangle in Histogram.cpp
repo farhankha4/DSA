572914192b6bae571a1fbb3804352ba9faa2a756
farhankha4 <farhankha2020@gmail.com>
2026-02-04 15:20:44 +0530

    largest rectangle in histogram

---
+++ stack/84. Largest Rectangle in Histogram.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int maxArea = 0;
+        int n = heights.size();
+        for(int i = 0; i<=n;i++){
+            while(!st.empty() && (i==n || heights[st.top()]>heights[i])){
+                int element = heights[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()){
+                    width = i;
+                }
+                else{
+                    width = i -  st.top() -1;
+                }
+                maxArea = max(maxArea,(width * element));
+            }
+            st.push(i);
+        }
+        return maxArea;
+    }
+};
