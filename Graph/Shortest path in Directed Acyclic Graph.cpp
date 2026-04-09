ce80173e8e9b7a9f31633263658e96bfb832721f
farhankha4 <farhankha2020@gmail.com>
2026-04-09 15:21:57 +0530

    shortest path in directed acyclic graph

---
+++ Graph/Shortest path in Directed Acyclic Graph.cpp
@@ -0,0 +1,54 @@
+//gfg problem
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    void toposort(int node, vector<vector<pair<int,int>>> &adj,vector<int>& vis, stack<int>& st){
+        vis[node] = 1;
+        for(auto it:adj[node]){
+            int v = it.first;
+            if(!vis[v]){
+                toposort(v,adj,vis,st);
+            }
+        }
+        st.push(node);
+    }
+  public:
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> adj(V);
+        for(int i = 0 ; i < E; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+            adj[u].push_back({v,w});
+        }
+        vector<int> vis(V,0);
+        stack<int> st;
+        for(int i = 0; i < V; i++){
+            if(!vis[i]){
+                toposort(i,adj,vis,st);
+            }
+        }
+        vector<int> dist(V);
+        for(int i = 0; i < V; i++){
+            dist[i] = 1e9;
+        }
+        dist[0] = 0;
+        while(!st.empty()){
+            int node = st.top();
+            st.pop();
+            for(auto it: adj[node]){
+                int v = it.first;
+                int w = it.second;
+                if(dist[node] != 1e9 && dist[node] + w < dist[v]){
+                    dist[v] = dist[node] + w;
+                }
+            }
+        }
+        for(int i = 0; i < V; i++){
+            if(dist[i] == 1e9){
+                dist[i] = -1;
+            }
+        }
+        return dist;
+    }
+};
