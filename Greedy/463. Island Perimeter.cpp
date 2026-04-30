f796e5c75fbd463c7920448a7aaba4c0d81f38d1
farhankha4 <farhankha2020@gmail.com>
2026-04-30 19:54:59 +0530

    island perimeter

---
+++ Greedy/463. Island Perimeter.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int parameter = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j]==1){
+                    parameter += 4;
+                    if(i > 0 && grid[i-1][j] == 1){
+                        parameter -= 2;
+                    }
+                    if(j > 0 && grid[i][j-1] == 1){
+                        parameter -= 2;
+                    }
+                }
+            }
+        }
+        return parameter;
+    }
+};
