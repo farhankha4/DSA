f83322cf1bde237a96b0f35336f87b7b08a143de
farhankha4 <farhankha2020@gmail.com>
2026-05-19 00:00:16 +0530

    k closest points to origin

---
+++ Heap/973. K Closest Points to Origin.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,int>> pq;
+        int n = points.size();
+        for(int i = 0; i < n; i++){
+            int x = points[i][0];
+            int y = points[i][1];
+            int dis = x*x + y*y;
+            pq.push({dis,i});
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        vector<vector<int>> ans;
+        while(!pq.empty()){
+            int index = pq.top().second;
+            pq.pop();
+            ans.push_back(points[index]);
+        }
+        return ans;
+    }
+};
