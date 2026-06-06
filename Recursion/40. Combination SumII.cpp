2fe6e213230674e74783fe69ca7910e20f91b622
farhankha4 <farhankha2020@gmail.com>
2026-06-06 19:52:23 +0530

    combination sum II

---
+++ Recursion/40. Combination SumII.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public: 
+    void combination(int ind, int target, vector<int>& candidates, vector<vector<int>>& ans, vector<int> ds){
+        if(target == 0){
+            ans.push_back(ds);
+        }
+        for(int i = ind; i < candidates.size(); i++){
+            if(i > ind && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+            ds.push_back(candidates[i]);
+            combination(i+1 , target - candidates[i], candidates,ans,ds);
+            ds.pop_back();
+        }
+
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(candidates.begin(),candidates.end());
+        combination(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
